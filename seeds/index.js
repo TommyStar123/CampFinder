3097b82c33b485094d2e5fa71a34a81b9e6ef8c0
TommyStar123 <67210363+TommyStar123@users.noreply.github.com>
2022-08-22 15:58:57 -0400

    Seeding Data

---
+++ seeds/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const cities = require('./cities')
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/YelpCamp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log('Database connected')
+})
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random].city, cities[random].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close()
+})
