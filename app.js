cae9967f66a5370d482d5a28f3511ef557e371ea
TommyStar123 <67210363+TommyStar123@users.noreply.github.com>
2022-08-30 22:23:04 -0400

    add start script

--- app.js
+++ app.js
@@ -99,7 +99,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log("listening");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 })
 
